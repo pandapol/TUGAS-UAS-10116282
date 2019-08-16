954221b87af839d7c608e14ace9fee231bf59a1f
pandapol <arviramadhan@email.unikom.ac.id>
2019-08-16 19:43:06 +0700

    7th Commit

--- app/src/main/java/com/pandapol/kontakteman/data/model/FriendsData.java
+++ app/src/main/java/com/pandapol/kontakteman/data/model/FriendsData.java
@@ -8,6 +8,10 @@ package com.pandapol.kontakteman.data.model;
 
 public class FriendsData {
     public static String[][] initFriendsData = new String[][]{
-            {"Arvi Ramadhan", "10116282", "IF-7", "082215593717", "arviramadhan@email.unikom.ac.id", "@mhabibnf"}
+            {"Arvi Ramadhan", "10116282", "IF-7", "082215593717", "arviramadhan@email.unikom.ac.id", "@a.pandapol"},
+            {"Aditia Julianto", "10116280", "IF-7", "082249031347", "kweeadit@gmail.com","@julian"},
+            {"Jordan Kelvin", "10116277","IF-7","08227381921","jordankelvin129@gmail.com","@jordankhelvin"},
+            {"Muhammad Adityo Fathur Rahim", "10116315", "IF-7", "087824264991", "adityofr9@gmail.com","@adityo_fr"},
+            {"Andika Dirgantara", "10116291","IF-7","082291642657","andikadirgantara1@gmail.com", "@andikacikke"}
     };
 }
